04c67223a1e93d24d79bbbd97edc4be217f469b1
Macley-Kun <wesley.de.vree@gmail.com>
2019-08-31 10:21:13 +0200

    get empty config files

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,7 @@
-# Cleaning  build artifacts
+# Cleaning build artifacts
 mv Watchdog.jar ../
 rm -r *
 mv ../Watchdog.jar .
+# Adding in empty config files
+curl -o config.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/config.yml
+curl -o constants.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/constants.yml
