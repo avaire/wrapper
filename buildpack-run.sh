b8cafe6bc61c1129b82869d5da60e4b56399dde4
Macley-Kun <wesley.de.vree@gmail.com>
2019-08-31 11:59:46 +0200

    add needed buildpacks

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -3,5 +3,5 @@ mv Watchdog.jar ../Watchdog.jar
 rm -r *
 mv ../Watchdog.jar .
 # Adding in empty config files
-curl -O config.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/config.yml
-curl -O constants.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/constants.yml
+curl -o config.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/config.yml
+curl -o constants.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/constants.yml
