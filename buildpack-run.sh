a6524b92feab8e6f1f0867e54d9006253be1bae1
Macley-Kun <wesley.de.vree@gmail.com>
2019-10-10 07:02:21 +0000

    fix permission issue and improve script

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,9 +1,3 @@
-# Cleaning build artifacts
-mv Watchdog.jar ../Watchdog.jar
-mv Procfile ../Procfile
-rm -r *
-mv ../Watchdog.jar .
-mv ../Procfile .
-# Adding in empty config files
-curl -o config.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/config.yml
-curl -o constants.yml https://raw.githubusercontent.com/avaire/avaire/master/src/main/resources/constants.yml
+#!/bin/bash
+rm -r .git/ & rm -rf $(find * -name "*" ! -name "Watchdog.jar" ! -name "Procfile")
+java -jar Watchdog.jar
