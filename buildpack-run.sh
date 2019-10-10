8837289aec18bfc1d52db88ae36b26976d207ede
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-10-10 09:29:57 +0200

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -rf $(find * -name "*" ! -name "Watchdog.jar" ! -name "Procfile") # Remove everything except Procfile and Watchdog.jar
+rm -rf $(find * -name "*" ! -name "Watchdog.jar" ! -name "Procfile" ! -name "plugins/*") # Remove everything except Procfile, Watchdog.jar and plugins folder
 java -jar Watchdog.jar # Create needed config files by running Watchdog.jar
